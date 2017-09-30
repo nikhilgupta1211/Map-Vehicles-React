06085389a6ce40cc4f758a216c88d04665e4e503
nikhilgupta1211 <nikeythe@gmail.com>
2017-09-30 20:48:50 +0530

    dipper

--- src/components/map.js
+++ src/components/map.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Input from './input';
 
 const google = window.google;
 class Map extends Component {
   constructor(props) {
     super(props);
-    this.pSubmit = this.pSubmit.bind(this);
-    this.drawPath = this.drawPath.bind(this);
+    this.traceVehicle = this.traceVehicle.bind(this);
+    this.subCords = this.subCords.bind(this);
     this.moveMarker = this.moveMarker.bind(this);
     this.state = { cords: [], centerLong: 28.589664, centerLat: 77.05742 }
   }
@@ -18,7 +17,7 @@ class Map extends Component {
   renderMap(){
     this.map = new google.maps.Map(this.refs.map, {
           center: {lat: this.state.centerLong, lng: this.state.centerLat},
-          zoom: 14
+          zoom: 9
         });
   }
 
@@ -33,7 +32,7 @@ class Map extends Component {
     return marker;
   }
 
-  pSubmit(cords){
+  traceVehicle(cords,color){
     this.setState({ cords: cords, centerLong: cords.srcLong, centerLat: cords.srcLat });
     this.map.panTo({lat: cords.srcLong, lng: cords.srcLat});
 
@@ -50,28 +49,28 @@ class Map extends Component {
       travelMode: google.maps.TravelMode.DRIVING,
     }, (result, status) => {
       if (status === google.maps.DirectionsStatus.OK) {
-          this.drawPath(result.routes[0].overview_path,this.map);
-          this.moveMarker(result.routes[0].overview_path)
+          this.drawPath(result.routes[0].overview_path,this.map,color);
+          this.moveMarker(result.routes[0].overview_path,color)
       } else {
         console.error(`error fetching directions ${result}`);
       }
     });
   }
 
-  drawPath(cords,map){
+  drawPath(cords,map,color){
     var flightPlanCoordinates = cords;
          var flightPath = new google.maps.Polyline({
            path: flightPlanCoordinates,
            geodesic: true,
-           strokeColor: '#323232',
-           strokeOpacity: 0.7,
+           strokeColor: color,
+           strokeOpacity: 0.5,
            strokeWeight: 4
          });
 
          flightPath.setMap(map);
   }
 
-  moveMarker(cords)
+  moveMarker(cords,color)
   {   
       var marker = this.addMarker(cords[0],'Vehicle','','markcar.png');
       var count = 0;
@@ -82,17 +81,34 @@ class Map extends Component {
         }
         count = (count + 1);
         marker.setPosition( cords[count] );
-        that.drawPath([cords[count-1],cords[count]],that.map);
-        if(count%15===0)
-          that.map.panTo({lat: cords[count].lat(), lng: cords[count].lng()});
+        that.drawPath([cords[count-1],cords[count]],that.map,color);
       }, 100);
+  }
+
+  subCords(points,color){
+    var cords = {
+      srcLong: points[0],
+      srcLat: points[1],
+      destLong: points[2],
+      destLat: points[3]
+      };
+    this.traceVehicle(cords,color);
   }
 
   render(){
     return(
       <section>
-        <Input pSubmit={this.pSubmit}/><br />
-        <div ref="map" className="map">I should be a map.</div>
+        <div ref="map" className="map">I should be a map.</div><br />
+        <button 
+        className="btn btn-primary btn_vehicle"
+        onClick={() => this.subCords([27.1767,78.0081,28.4595,77.0266],'#0282c8')}>
+        Vehicle 1
+        </button>
+        <button 
+        className="btn btn-primary"
+        onClick={() => this.subCords([29.3909,76.9635,27.8974,78.0880],'#e12525')}>
+        Vehicle 2
+        </button>
       </section>
     );
   }
